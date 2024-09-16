5f59147e51468022e5c5b187dcf60c38bd260a08
Santiago Dobón <santifinland@gmail.com>
2024-09-16 10:38:36 +0200

    [FEATURE] Only protected when click

--- projects/keycloak-react/src/App.tsx
+++ projects/keycloak-react/src/App.tsx
@@ -1,40 +1,11 @@
-import { useState, useEffect } from "react";
-import Keycloak from "keycloak-js";
 
 import './App.css';
-import Protected from "./components/Protected";
-import Public from "./components/Public";
+import Home from "./components/Home";
 
 
 const App: React.FC = () => {
 
-
-  const keycloakOptions = {
-    url: import.meta.env.VITE_KEYCLOAK_URL,
-    realm: import.meta.env.VITE_KEYCLOAK_REALM,
-    clientId: import.meta.env.VITE_KEYCLOAK_CLIENT,
-  };
-
-  const [keycloak, setKeycloak] = useState<Keycloak>()
-
-  useEffect(() => {
-    const initKeycloak = async () =>{
-      const keycloakInstance = new Keycloak(keycloakOptions)
-      try {
-        await keycloakInstance.init({onLoad: 'login-required'})
-        setKeycloak(keycloakInstance)
-        if (keycloakInstance.authenticated){
-          console.log("Exxxitooooorr")
-          console.log(keycloakInstance)
-        }
-      } catch (error) {
-        console.log(`Error ${error}`)
-      }
-    }
-    initKeycloak()
-  }, [])
-
-  return keycloak ? <Protected /> : <Public />;
+  return <Home />
 };
 
 export default App;
