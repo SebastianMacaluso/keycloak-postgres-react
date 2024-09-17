ec759b90c065ebeeef173bfcdb20ee1964a504c0
Santiago Dobón <santifinland@gmail.com>
2024-09-17 12:43:01 +0200

    [FEATURE] Add css

--- db/backup.sh
+++ db/backup.sh
@@ -6,4 +6,4 @@ cd /tmp || exit
 docker exec -t postgres-container pg_dump -U keycloak -d keycloakdb -F c -b -v -f /var/lib/postgresql/data/keycloakdb_backup.dump
 
 # From the host machine to retrieve the created backup that has been stored inside the running container
-docker cp postgres-container:/var/lib/postgresql/data/keycloakdb_backup.dump keycloakdb_backup.dump
+docker cp postgres-container:/var/lib/postgresql/data/keycloakdb_backup.dump /tmp/keycloakdb_backup.dump
